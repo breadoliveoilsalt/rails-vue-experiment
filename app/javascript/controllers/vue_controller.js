c6eaaede7d18674f699a9702a5149279c2eaf5cb
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2022-04-19 17:42:59 -0400

    get something to render

--- app/javascript/controllers/vue_controller.js
+++ app/javascript/controllers/vue_controller.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     data() {
         return { count: 1 }
     },
+    template: `<div>count is {{ count }}</div>`,
     created() {
         console.log("count is: " + this.count) // => "count is: 1"
     }
