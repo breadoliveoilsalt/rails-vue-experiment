5d5fa0f160eaf29eec269b805d956f6720a5f256
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2022-04-20 12:54:18 -0400

    Got another file to render!

--- app/javascript/controllers/vue_controller.js
+++ app/javascript/controllers/vue_controller.js
@@ -1,15 +1,18 @@
 import * as Vue from "vue"
+import MyComponent from './my_component.js'
 
 console.log('Made it to here')
+console.log('import seems to be working')
 
 const entryPoint = "#app"
-const app = Vue.createApp({
-    data() {
-        return { count: 1 }
-    },
-    template: `<div>count is {{ count }}</div>`,
-    created() {
-        console.log("count is: " + this.count) // => "count is: 1"
-    }
-})
-app.mount(entryPoint)
+// const app = Vue.createApp({
+//     data() {
+//         return { count: 1 }
+//     },
+//     template: `<div>count is {{ count }}</div>`,
+//     created() {
+//         console.log("count is: " + this.count) // => "count is: 1"
+//     }
+// })
+// app.mount(entryPoint)
+Vue.createApp(MyComponent).mount(entryPoint)
